954a3abe7832d19ff63399523eff1bab16b785d7
John Besco <johnbesco.dev@gmail.com>
2023-05-04 11:36:11 +0100

    Add subtract function

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-function add (x, y) {
+let x = '20'
+let y = '2'
+
+function sumOf (x, y) {
     return (x + y);
 }
 
-let sumResult = add(x, y);
+let sumResult = sumOf(x, y);
+
+function subtract (x, y) {
+    return (x - y);
+}
+
+let subtractResult = subtract(x, y);
 
