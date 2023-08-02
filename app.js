58614b802a132fb09cc71db0df7e16a5d6496525
SirTokenSol <johnbesco.dev@gmail.com>
2023-08-02 22:28:52 +0100

    Remove all javascript and value tags in html file

--- app.js
+++ app.js
@@ -1,80 +1,18 @@
-let num1;
-let num2;
-let operator;
+num1 = 2;
+num2 = 2;
 
-function sumOf (num1, num2) {
-    return (num1 + num2);
+function add() {
+    return num1 + num2
 }
 
-let sumResult = sumOf(num1, num2);
-
-function subtract (num1, num2) {
-    return (num1 - num2);
+function subtract() {
+    return num1 - num2
 }
 
-let subtractResult = subtract(num1, num2);
-
-function divide (num1, num2) {
-    return (num1 / num2);
+function divide() {
+    return num1 / num2
 }
 
-let divideResult = divide(num1, num2);
-
-function multiply (num1, num2) {
-    return (num1 * num2);
-}
-
-let multiplyResult = multiply(num1, num2);
-
-
-let operate = function(operator, num1, num2) {
-    if (operator == '+') {
-        return sumOf(num1, num2);
-    } else if (operator == '-') {
-        return subtract(num1, num2);
-    } else if (operator == '/') {
-        return divide(num1, num2)
-    } else if (operator == '*') {
-        return multiply(num1, num2);
-    }
-} 
-
-const display = document.querySelector('#main-display');
-const paragraph = document.createElement('p');
-const btn = document.querySelectorAll('.numpad');
-const operatorBtn = document.querySelectorAll('.operators');
-const equalBtn = document.querySelector('.equal');
-
-let currentNumber = '';
-let storedNumber = '';
-let selectedOperator = '';
-
-btn.forEach(button => {
-    button.addEventListener('click', () => {
-        const number = button.value;
-        currentNumber += number;
-        console.log(button.value);
-        updateDisplay();
-    });
-});
-
-operatorBtn.forEach(operatorButton => {
-    operatorButton.addEventListener('click', () => {
-        selectedOperator = operatorButton.value;
-        storedNumber = currentNumber;
-        currentNumber = '';
-        updateDisplay();
-    });
-});
-
-equalBtn.addEventListener('click', () => {
-    const result = operate(selectedOperator, parseFloat(storedNumber), parseFloat(currentNumber));
-    currentNumber = result.toString();
-    storedNumber = '';
-    selectedOperator = '';
-    updateDisplay();
-});
-
-function updateDisplay() {
-    display.textContent = currentNumber;
+function multiply() {
+    return num1 * num2
 }
