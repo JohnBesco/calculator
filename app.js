a311fb05d92d77732513d9632681ecdfa0135d80
SirTokenSol <johnbesco.dev@gmail.com>
2023-08-02 22:50:10 +0100

    add event handlers to numpad buttons

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
-num1 = 2;
-num2 = 2;
+class Calculator {
+    constructor() {
+        this.display = document.getElementById('main-display');
+        this.clearButton = document.getElementById('clear-btn');
+        this.deleteButton = document.getElementById('delete-btn');
+        this.numberButtons = document.querySelectorAll('.numpad');
+        this.operatorButtons = document.querySelectorAll('.operators');
+        this.equalButton = document.querySelector('.equal');
+        this.decimalButton = document.getElementById('decimal');
 
-function add() {
-    return num1 + num2
-}
+        this.currentInput = '0';
+        this.currentOperator = null;
+        this.previousInput = '0';
 
-function subtract() {
-    return num1 - num2
-}
-
-function divide() {
-    return num1 / num2
-}
-
-function multiply() {
-    return num1 * num2
+        this.attachEventHandlers();
+    }
 }
